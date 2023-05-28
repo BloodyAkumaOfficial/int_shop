9b8c9fb1db23725ebd4d5199d94f3590e0a9db45
oni <onisama228@gmail.com>
2023-05-28 22:00:42 +0200

    get start dev server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server was started on port: ${PORT}`));
