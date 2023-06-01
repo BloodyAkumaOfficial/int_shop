0aa1cfd275908d1e3bc5568f73816a1ccf3e5979
oni <onisama228@gmail.com>
2023-06-01 15:06:09 +0200

    create models of database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 require('dotenv').config();
 const sequelize = require('./db');
+const models = require('./models/models')
+const cors = require('cors');
 
 const PORT = process.env.PORT || 6000;
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'I`ts work!!'});
+})
 
 const start = async () => {
       try {
