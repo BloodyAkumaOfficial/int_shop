a26f986cc4c668723b79aba2103969016f2c5216
oni <onisama228@gmail.com>
2023-06-01 14:01:24 +0200

    create connect to database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
-import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config()
+const express = require('express');
+require('dotenv').config();
+const sequelize = require('./db');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server was started on port: ${PORT}`));
+const start = async () => {
+      try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`Server was started on port: ${PORT}`));
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+start().catch(e => console.log(e.message));
