7d0cace3cfb401816a42f8597711c625f74d6ca4
oni <onisama228@gmail.com>
2023-06-02 23:44:36 +0200

    update crud

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,5 @@
+const ApiError = require('../error/ApiError');
+
 class UserController {
     async registration (req, res) {
 
@@ -5,8 +7,12 @@ class UserController {
     async login (req, res) {
 
     }
-    async check (req, res) {
-
+    async check (req, res, next) {
+        const {id} = req.query;
+        if (!id) {
+            return next(ApiError.bedRequest('id was not specified!'));
+        }
+        res.json(id);
     }
 }
 
