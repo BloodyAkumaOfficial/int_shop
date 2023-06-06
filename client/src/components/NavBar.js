91bacf91c28d3e83fa4691e984717db7dfd93b01
oni <onisama228@gmail.com>
2023-06-06 23:43:30 +0200

    create device page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React, {useContext} from 'react';
 import {Context} from "../index";
 import {Navbar, Container, Nav, Button} from "react-bootstrap";
-import {NavLink} from "react-router-dom";
-import {SHOP_ROUTE} from "../utils/consts";
+import {NavLink, useHistory} from "react-router-dom";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
 
 const NavBar = observer( () => {
     const {user} = useContext(Context);
+    const history = useHistory()
 
     return (
             <Navbar bg="dark" variant="dark">
@@ -14,12 +15,26 @@ const NavBar = observer( () => {
                     <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>My Online Shop</NavLink>
                     {user.isAuth ?
                         <Nav className="ml-auto" style={{color: 'white'}}>
-                            <Button variant={'outline-light'}>Admin Panel</Button>
-                            <Button variant={'outline-light'} className="mx-lg-3">Log Out</Button>
+                            <Button
+                                variant={'outline-light'}
+                                onClick={() => history.push(ADMIN_ROUTE)}
+                            >
+                                Admin Panel
+                            </Button>
+                            <Button
+                                variant={'outline-light'}
+                                className="mx-lg-3"
+                                onClick={() => {
+                                    history.push(LOGIN_ROUTE);
+                                    user.setIsAuth(false);
+                                }}
+                            >
+                                Log Out
+                            </Button>
                         </Nav>
                         :
                         <Nav className="ml-auto" style={{color: 'white'}}>
-                            <Button variant={'outline-light'} onClick={() => user.setIsAuth(true)}>Authorization</Button>
+                            <Button variant={'outline-light'} onClick={() => user.setIsAuth(true)}>Sign in</Button>
                         </Nav>
                     }
                 </Container>
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
+import bigStar from '../assets/big_star.png'
 
 const DevicePage = () => {
-    return (
-        <div>
-            DevicePage
-        </div>
+    const device = {id: 1, name: '12 pro', price: 1200, rating: 5, img: 'https://apiua.icoola.ua/aimeos/1.d/files/a/e/aedacf71_apple-iphone-12-pro-256gb-graphite-icoola-1.jpg'};
+    const description = [
+        {id: 1, name: 'test', description: 'qwertyyyyyyyyyy'},
+        {id: 2, name: 'test', description: 'qwertyyyyyyyyyy'},
+        {id: 3, name: 'test', description: 'qwertyyyyyyyyyy'},
+        {id: 4, name: 'test', description: 'qwertyyyyyyyyyy'},
+        {id: 5, name: 'test', description: 'qwertyyyyyyyyyy'},
+        {id: 6, name: 'test', description: 'qwertyyyyyyyyyy'},
+    ]
+
+        return (
+        <Container className='mt-3'>
+            <Row>
+                <Col md={4}>
+                    <Image height={300} width={300} src={device.img}/>
+                </Col>
+                <Col md={4}>
+                    <Row className='d-flex flex-column align-items-center'>
+                        <h2 style={{textAlign: 'center'}}>{device.name}</h2>
+                        <div
+                            className='d-flex justify-content-center align-items-center'
+                            style={{background: `url(${bigStar}) no-repeat center center`, width: 240, height: 240, backgroundSize: 'cover', textAlign: 'center', fontSize: 50}}
+                        >
+                            {device.rating}
+                        </div>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className='d-flex flex-column align-items-center justify-content-around'
+                        style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgrey'}}
+                    >
+                        <h3>{device.price} $</h3>
+                        <Button variant={'outline-dark'}>Add to basket</Button>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className='d-flex flex-column mt-3'>
+                <h3>Device info</h3>
+                {description.map((info, index) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgrey' : 'transparent', padding: 10}}>
+                        {info.name}: {info.description}
+                    </Row>
+                )}
+            </Row>
+        </Container>
     );
 };
 
